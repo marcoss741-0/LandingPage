6ec193d50387592538f6e01a937b476024c3932f
marcoss741-0 <marcossn741@icloud.com>
2025-02-08 08:16:35 -0300

    Codigo redundante corrrigido

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,9 @@
 var mobileMenu = document.querySelector(".mobile-menu");
 var navList = document.querySelector(".nav-list");
 
-mobileMenu.addEventListener("click", function () {
+const toggleMenu = () => {
   navList.classList.toggle("active");
   mobileMenu.classList.toggle("active");
-});
+};
 
-navList.addEventListener("click", function () {
-  navList.classList.remove("active");
-});
+mobileMenu.addEventListener("click", toggleMenu);
