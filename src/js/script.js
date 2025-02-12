24d7ec8c9d8739590e051254756c48b343856283
marcoss741-0 <marcossn741@icloud.com>
2025-02-12 15:38:26 -0300

    Testes

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,20 @@ const toggleMenu = () => {
 };
 
 mobileMenu.addEventListener("click", toggleMenu);
+
+// Seleciona os elementos
+const openFloatingBoxButton = document.getElementById('openFloatingBox');
+const closeFloatingBoxButton = document.getElementById('closeFloatingBox');
+const floatingBox = document.getElementById('floatingBox');
+
+// Abrir caixa com animação
+openFloatingBoxButton.addEventListener('click', () => {
+  floatingBox.style.display = 'block';
+  setTimeout(() => floatingBox.classList.add('active'), 10);
+});
+
+// Fechar caixa com animação
+closeFloatingBoxButton.addEventListener('click', () => {
+  floatingBox.classList.remove('active');
+  setTimeout(() => floatingBox.style.display = 'none', 300);
+});
